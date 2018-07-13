5d21e8659555e1a3cb649a7143e6d33e4145052a
Nikki Tran <nikkitran7898@gmail.com>
2018-07-13 14:49:14 -0700

    Added xcode project

---
+++ Nooves/Nooves/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nooves
+//
+//  Created by Nikki Tran on 7/13/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nooves/Nooves/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nooves
+//
+//  Created by Nikki Tran on 7/13/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
