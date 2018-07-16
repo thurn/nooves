00ff7919e82d36857c6651309cd9ffa8c840722e
Norette Ingabire <noretteingabire@gmail.com>
2018-07-16 11:52:13 -0700

    timeline and compose controllers

---
+++ Nooves/Nooves/Assets.xcassets/ComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.h
+//  Nooves
+//
+//  Created by Norette Ingabire on 7/16/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeViewController : UIViewController
+
+@end
