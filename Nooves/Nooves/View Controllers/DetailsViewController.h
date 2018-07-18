65df77ff963c9def1747029cc2006fa6c1f8ba08
Nikki Tran <nikkitran7898@gmail.com>
2018-07-18 15:37:04 -0700

    Added details view file

---
+++ Nooves/Nooves/View Controllers/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  Nooves
+//
+//  Created by Nikki Tran on 7/18/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ Nooves/Nooves/View Controllers/DetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.m
+//  Nooves
+//
+//  Created by Nikki Tran on 7/18/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
