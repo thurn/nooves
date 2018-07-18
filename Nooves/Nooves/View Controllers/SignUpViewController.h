26f0486ca91a96c3f736598f264a5ae0ff441538
Nikki Tran <nikkitran7898@gmail.com>
2018-07-18 15:39:54 -0700

    Added sign up file

---
+++ Nooves/Nooves/View Controllers/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Nooves
+//
+//  Created by Nikki Tran on 7/18/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
