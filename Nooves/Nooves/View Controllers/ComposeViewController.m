8ca684a7aab67a61f9963061c58dcf828a8f6800
Nikki Tran <nikkitran7898@gmail.com>
2018-07-23 11:00:52 -0700

    formatted compose view buttons

--- Nooves/Nooves/View Controllers/ComposeViewController.m
+++ Nooves/Nooves/View Controllers/ComposeViewController.m
@@ -14,40 +14,45 @@
 #import "CategoryPickerPopUpViewController.h"
 
 @interface ComposeViewController () <UIScrollViewDelegate, UITextViewDelegate>
+
 @end
 
 @implementation ComposeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     if(!self.tempPostsArray){
         self.tempPostsArray = [[NSMutableArray alloc] init];
     }
+    
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationController.navigationBarHidden = NO;
     self.navigationItem.title = @"New Event";
 
+    // set up event title properties
     self.eventTitle = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 1000, 150)];
     self.eventTitle.text = nil;
     self.eventTitle.placeholder = @"Event name";
     self.eventTitle.borderStyle = UITextBorderStyleRoundedRect;
     self.eventTitle.textColor = UIColor.grayColor;
-
-    // need to connect actual locations from api
-    self.eventLocation = [[UITextField alloc] initWithFrame:CGRectMake(0, 150, 1000, 150)];
-    self.eventLocation.text = nil;
-    self.eventLocation.placeholder = @"Add location";
-    self.eventLocation.borderStyle = UITextBorderStyleRoundedRect;
-    self.eventLocation.textColor = UIColor.grayColor;
     
+    // set location display label properties
+    UILabel *locationLabel = [[UILabel alloc] init];
+    NSString *locationColon = @"Location: ";
+    locationLabel.frame = CGRectMake(10, 440, 100, 100);
+    locationLabel.text = [locationColon stringByAppendingString:@"test"];
+    [locationLabel sizeToFit];
+    
+    // set activity display label properties
     UILabel *activityLabel = [[UILabel alloc] init];
     NSString *activityColon = @"Category: ";
     activityLabel.frame = CGRectMake(10, 420, 150, 150);
     NSString *activity = [Post activityTypeToString:self.activityType];
     activityLabel.text = [activityColon stringByAppendingString:activity];
     [activityLabel sizeToFit];
-    
 
+    // set date display label properties
     UILabel *dateLabel = [[UILabel alloc] init];
     NSString *dateColon = @"Date: ";
     dateLabel.frame = CGRectMake(10, 500, 100, 100);
@@ -64,38 +69,46 @@
     [dateLabel sizeToFit];
     [self.scrollView addSubview:dateLabel];
     
-    self.eventDescription = [[UITextView alloc] initWithFrame:CGRectMake(0, 300, 100, 150)];
+    // set event description properties
+    self.eventDescription = [[UITextView alloc] initWithFrame:CGRectMake(0, 150, 1000, 150)];
     self.eventDescription.delegate = self;
     self.eventDescription.text = @"Add a description";
     self.eventDescription.textColor = UIColor.grayColor;
 
+    // set scroll view properties
     self.scrollView.translatesAutoresizingMaskIntoConstraints = NO;
     self.scrollView.backgroundColor = [UIColor brownColor];
-
     self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    
     CGFloat descHeight = self.eventDescription.frame.origin.y + self.eventDescription.frame.size.height+1000.0;
     self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, descHeight);
-
-    // self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds, self.scrollView.bounds);
+    
     CGRect contentRect = CGRectZero;
 
     for (UIView *view in self.scrollView.subviews) {
         contentRect = CGRectUnion(contentRect, view.frame);
     }
     self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    
+    // add scroll view to subview
     [self.view addSubview:self.scrollView];
     
+    // add components to scroll view
     [self.scrollView addSubview:self.eventTitle];
-    [self.scrollView addSubview:self.eventLocation];
     [self.scrollView addSubview:self.eventDescription];
     [self.scrollView addSubview:activityLabel];
     [self.scrollView addSubview:[self selectDate]];
     [self.scrollView addSubview:dateLabel];
     [self.scrollView addSubview:[self selectCategory]];
+    [self.scrollView addSubview:[self selectLocation]];
+    [self.scrollView addSubview:locationLabel];
+    
+    // add tab bar buttons to controller
     [self postButton];
     [self goBack];
 }
 
+// checks to see if user is editing event description and changes text color if true
 - (void) textViewDidBeginEditing:(UITextView *)textView {
     if (textView.textColor == UIColor.grayColor) {
         textView.text = nil;
@@ -103,6 +116,7 @@
     }
 }
 
+// if event description is nil continue setting text color to grey
 - (void) textViewDidEndEditing:(UITextView *)textView {
     if (textView.text == nil) {
         textView.text = @"Add description";
@@ -110,23 +124,17 @@
     }
 }
 
-- (UIBarButtonItem *) postButton {
-    UIBarButtonItem *postButton = [[UIBarButtonItem alloc] init];
-    postButton.title = @"Share";
-    postButton.target = self;
-    postButton.action = @selector(didTapPost);
-    self.navigationItem.rightBarButtonItem = postButton;
-    return postButton;
-}
-
+// set up select date button properties
 -(UIButton *)selectDate{
     UIButton *selectDate = [UIButton buttonWithType:UIButtonTypeSystem];
-    [selectDate setTitle:@"Select Date" forState:UIControlStateNormal];
+    UIImage *calendarIcon = [UIImage imageNamed:@"calendar"];
+    [selectDate setImage:calendarIcon forState:UIControlStateNormal];
     [selectDate addTarget:self action:@selector(didSelectDate) forControlEvents:UIControlEventTouchUpInside];
     [selectDate sizeToFit];
     return selectDate;
 }
 
+// pass post data and jump to date picker view
 -(void)didSelectDate{
     DatePickerPopUpViewController *datePicker = [DatePickerPopUpViewController new];
     datePicker.tempPostsArray = self.tempPostsArray;
@@ -135,15 +143,40 @@
     [self.navigationController pushViewController:datePicker animated:YES];
 }
 
+// set up select location properties
+-(UIButton *)selectLocation{
+    UIButton *selectLocation = [UIButton buttonWithType:UIButtonTypeSystem];
+    UIImage *locationIcon = [UIImage imageNamed:@"location-marker"];
+    [selectLocation setImage:locationIcon forState:UIControlStateNormal];
+    //[selectLocation addTarget:self action:@selector(didSelectLocation) forControlEvents:UIControlEventTouchUpInside];
+    selectLocation.center = CGPointMake(20, 0);
+    [selectLocation sizeToFit];
+    return selectLocation;
+}
+
+// pass post data and jump to location picker view
+-(void)didSelectLocation{
+    // LocationPickerPopUpViewController *locationPicker = [LocationPickerPopUpViewController new];
+    //locationPicker.tempPostsArray = self.tempPostsArray;
+    //locationPicker.date = self.date;
+    //locationPicker.activityType = self.activityType;
+    //locationPicker.location = self.location;
+    //[self.navigationController pushViewController:locationPicker animated:YES];
+}
+
+// set up selection category button properties
 -(UIButton *)selectCategory{
     UIButton *selectCategory = [UIButton buttonWithType:UIButtonTypeSystem];
-    [selectCategory setTitle:@"Select Category" forState:UIControlStateNormal];
+    UIImage *tagIcon = [UIImage imageNamed:@"tags"];
+    [selectCategory setImage:tagIcon forState:UIControlStateNormal];
     [selectCategory addTarget:self action:@selector(didSelectCategory) forControlEvents:UIControlEventTouchUpInside];
-    selectCategory.center = CGPointMake(0, 40);
+    selectCategory.center = CGPointMake(50, 0);
     [selectCategory sizeToFit];
     return selectCategory;
 }
 
+
+// pass post data and jump to category picker view
 -(void)didSelectCategory{
     CategoryPickerPopUpViewController *categoryPicker = [CategoryPickerPopUpViewController new];
     categoryPicker.tempPostsArray = self.tempPostsArray;
@@ -152,6 +185,7 @@
     [self.navigationController pushViewController:categoryPicker animated:YES];
 }
 
+// set up back button properties
 - (UIBarButtonItem *) goBack {
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back-icon"]
                                                                    style:UIBarButtonItemStylePlain
@@ -162,6 +196,23 @@
   return backButton;
 }
 
+// jump back to root view controller
+- (void) didTapBack {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    NSLog(@"User pressed to go back");
+}
+
+// set up post button properties
+- (UIBarButtonItem *) postButton {
+    UIBarButtonItem *postButton = [[UIBarButtonItem alloc] init];
+    postButton.title = @"Share";
+    postButton.target = self;
+    postButton.action = @selector(didTapPost);
+    self.navigationItem.rightBarButtonItem = postButton;
+    return postButton;
+}
+
+// passes post data and adds to post array and jump back to timeline view
 - (void) didTapPost {
     // API call
     self.post = [[Post alloc] MakePost:self.date withTitle:self.eventTitle.text withDescription:self.eventDescription.text withType:self.activityType];
@@ -170,11 +221,6 @@
     timeline.tempPostsArray = self.tempPostsArray;
     [self.navigationController pushViewController:timeline animated:YES];
     NSLog(@"User posted successfully");
-}
-
-- (void) didTapBack {
-    [self.navigationController popToRootViewControllerAnimated:YES];
-    NSLog(@"User pressed to go back");
 }
 
 - (void)didReceiveMemoryWarning {
