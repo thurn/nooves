f4661c07515ec29c92c364b40bc12a85cf99b9c5
Nikki Tran <nikkitran7898@gmail.com>
2018-07-24 15:57:31 -0700

    location cell populates with search results

--- Nooves/Nooves/Views/LocationCell.m
+++ Nooves/Nooves/Views/LocationCell.m
@@ -12,29 +12,45 @@
 
 @implementation LocationCell
 
-- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier {
-    
-    // self = [super initWithStyle:nil reuseIdentifier:@"LocationCell"];
-    if (self) {
-        // configure control(s)
-        self.categoryImageView = [[UIImageView alloc] init];
-        self.addressLabel = [[UILabel alloc] init];
-        self.nameLabel = [[UILabel alloc] init];
-        self.location = [[NSDictionary alloc] init];
-        
-        [self addSubview:self.categoryImageView];
-        [self addSubview:self.addressLabel];
-        [self addSubview:self.nameLabel];
-    }
+-(instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    [self initialize];
     return self;
 }
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    [self initialize];
+    return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    
+    [self initialize];
+    return self;
+}
+
+- (void)initialize {
+    // initialize cell properties
+    self.categoryImageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 200, 100, 100)];
+    self.addressLabel = [[UILabel alloc] init];
+    self.nameLabel = [[UILabel alloc] init];
+    self.location = [[NSDictionary alloc] init];
+    
+    self.categoryImageView.frame = CGRectMake(0, 0, 20, 20);
+    
+    self.addressLabel.frame = CGRectMake(0, 30, 10, 10);
+    
+    self.nameLabel.frame = CGRectMake(0, 50, 10, 10);
+    
+    [self addSubview:self.categoryImageView];
+    [self addSubview:self.addressLabel];
+    [self addSubview:self.nameLabel];
 }
 
 - (void)updateWithLocation:(NSDictionary *)location {
