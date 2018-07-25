3b849d7e1d0bac31e82359f5fb67d8109b43665a
Norette Ingabire <noretteingabire@gmail.com>
2018-07-25 16:53:21 -0700

    renamed user

---
+++ Nooves/Nooves/Data Models/User.h
@@ -0,0 +1,18 @@
+//
+//  User.h
+//  Nooves
+//
+//  Created by Norette Ingabire on 7/25/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property(strong, nonatomic) NSString *name;
+@property(strong, nonatomic) NSString *biography;
+
+- (instancetype)initProfileWithInfo: (NSString *)userName withBio: (NSString *)bio;
+
+@end
