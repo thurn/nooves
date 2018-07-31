ef62988c20c83c3bb239ccbdb29fdd825bfa1176
Nkenna Aniedobe <nkenna@fb.com>
2018-07-31 14:49:54 -0700

    post details for things like when you are going

---
+++ Nooves/Nooves/View Controllers/PostDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostDetailsViewController.h
+//  Nooves
+//
+//  Created by Nkenna Aniedobe on 7/31/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostDetailsViewController : UIViewController
+
+@end
