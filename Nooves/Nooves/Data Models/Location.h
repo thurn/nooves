3be0e5807eae293170db6ad420a83680059a66e0
Nikki Tran <nikkitran7898@gmail.com>
2018-08-01 16:04:50 -0700

    location file created

---
+++ Nooves/Nooves/Data Models/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  Nooves
+//
+//  Created by Nikki Tran on 8/1/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ Nooves/Nooves/Data Models/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  Nooves
+//
+//  Created by Nikki Tran on 8/1/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
