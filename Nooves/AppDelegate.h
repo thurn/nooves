1f0106d2d4f51db967b9354a001ede0f8d38141b
Nkenna Aniedobe <nkenna@fb.com>
2018-08-02 14:34:38 -0700

    deleted duplicates

--- Nooves/AppDelegate.h
+++ Nooves/AppDelegate.h
@@ -1,5 +1,5 @@
 #import <UIKit/UIKit.h>
-#import <Firebase/Firebase.h>
+
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- View Controllers/TimelineViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-// TODO(Nikki): change timeline view controller to be a table view controller
-@interface TimelineViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-
-@property (strong, nonatomic) NSArray *firArray;
-
-@end
