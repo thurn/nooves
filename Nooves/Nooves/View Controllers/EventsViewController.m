93f74ce8ed346209810ca170e6fedd208e5d66f2
Norette Ingabire <noretteingabire@gmail.com>
2018-08-02 11:41:40 -0700

    event

--- Nooves/Nooves/View Controllers/EventsViewController.m
+++ Nooves/Nooves/View Controllers/EventsViewController.m
@@ -1,11 +1,3 @@
-//
-//  EventsViewController.m
-//  Nooves
-//
-//  Created by Norette Ingabire on 8/1/18.
-//  Copyright © 2018 Nikki Tran. All rights reserved.
-//
-
 #import "EventCell.h"
 #import "EventsViewController.h"
 
