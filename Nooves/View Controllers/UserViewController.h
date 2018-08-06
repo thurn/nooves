8f22f6fa388f2d29cab64e6b69e997d70f8ce075
Nkenna Aniedobe <nkenna@fb.com>
2018-08-06 12:33:38 -0700

    user view controller now complete

---
+++ Nooves/View Controllers/UserViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserViewController.h
+//  Nooves
+//
+//  Created by Nkenna Aniedobe on 8/6/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserViewController : UIViewController
+
+@end
