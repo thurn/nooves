b11bdb0ed7a530f2efe336e3af45fec26f244986
Nikki Tran <nikkitran7898@gmail.com>
2018-08-09 10:57:43 -0700

    added location settings file, configuring settings

---
+++ Nooves/View Controllers/LocationSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationSettingsViewController.h
+//  Nooves
+//
+//  Created by Nikki Tran on 8/9/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationSettingsViewController : UIViewController
+
+@end
---
+++ Nooves/View Controllers/LocationSettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  LocationSettingsViewController.m
+//  Nooves
+//
+//  Created by Nikki Tran on 8/9/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import "LocationSettingsViewController.h"
+
+@interface LocationSettingsViewController ()
+
+@end
+
+@implementation LocationSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
