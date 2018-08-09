fe672e68a421e77bc1e5d1b4191845d7635f1691
Nikki Tran <nikkitran7898@gmail.com>
2018-08-09 00:30:10 -0700

    fixed fscalendar and datepicker concatenation

--- Nooves/View Controllers/DatePickerModalViewController.m
+++ Nooves/View Controllers/DatePickerModalViewController.m
@@ -15,9 +15,12 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
+    self.gregorian = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    
     self.datepicker = [[UIDatePicker alloc] init];
     self.datepicker.frame = CGRectMake(0, 300, self.view.frame.size.width, 200);
     self.datepicker.timeZone = [NSTimeZone localTimeZone];
+//    self.datepicker.calendar = self.gregorian;
     self.datepicker.backgroundColor = [UIColor whiteColor];
     self.datepicker.datePickerMode = UIDatePickerModeTime;
     
@@ -32,7 +35,6 @@
     self.calendar.appearance.todayColor = [UIColor yellowColor];
     [self.view addSubview:calendar];
 
-    self.gregorian = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -46,17 +48,32 @@
     NSInteger day = [components day];
     
     NSDateFormatter *output = [[NSDateFormatter alloc] init];
-    [output setDateFormat:@"hh:mm a"];
-    NSString *string = [output stringFromDate:self.datepicker.date];
-    NSDate *dateFromString = [output dateFromString:string];
+    [output setDateFormat:@"MMM dd "];
     
-    self.selectedDate = [self.gregorian dateByAddingComponents:components toDate:dateFromString options:0];
+    NSDate *yeet = self.datepicker.date;
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    NSDateComponents *comp = [cal components:NSCalendarUnitHour | NSCalendarUnitMinute fromDate:yeet];
+    NSInteger hour = [comp hour];
+    NSInteger min = [comp minute];
+    
+    NSDateFormatter *output2 = [[NSDateFormatter alloc] init];
+    [output2 setDateFormat:@"hh:mm a"];
+    
+//    NSString *string = [output2 stringFromDate:self.datepicker.date];
+//    NSDate *dateFromString = [output2 dateFromString:string];
+    
+    self.selectedDate = [NSDate new];
+    self.selectedDate = [self.gregorian dateBySettingUnit:(NSCalendarUnitMonth) value:month ofDate:self.selectedDate options:0];
+    self.selectedDate = [self.gregorian dateBySettingUnit:(NSCalendarUnitDay) value:day ofDate:self.selectedDate options:0];
+    self.selectedDate = [self.gregorian dateBySettingUnit:(NSCalendarUnitHour) value:hour ofDate:self.selectedDate options:0];
+    self.selectedDate = [self.gregorian dateBySettingUnit:(NSCalendarUnitMinute) value:min ofDate:self.selectedDate options:0];
     
     NSDateFormatter *formatter = [NSDateFormatter new];
     formatter.dateFormat = @"MMM dd hh:mm a";
     formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US"];
     [formatter setTimeZone:[NSTimeZone localTimeZone]];
     NSString *dateDetails = [formatter stringFromDate:self.selectedDate];
+    //NSDate *help = [formatter dateFromString:dateDetails];
     NSLog(@"%@", dateDetails);
 }
 
