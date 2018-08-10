9c46d43c583abc5c9ff83614e8023eb4216d14cb
Nikki Tran <nikkitran7898@gmail.com>
2018-08-10 15:48:59 -0700

    new UILabel helper file + set global font

---
+++ Nooves/UILabel__Helper_+UILabel_Helper.m
@@ -0,0 +1,13 @@
+//
+//  UILabel__Helper_+UILabel_Helper.m
+//  Nooves
+//
+//  Created by Nikki Tran on 8/10/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import "UILabel__Helper_+UILabel_Helper.h"
+
+@implementation UILabel__Helper_ (UILabel_Helper)
+
+@end
