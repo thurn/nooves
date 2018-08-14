a3b8b45a56e15551736c69ed49fde9b85754c574
Nikki Tran <nikkitran7898@gmail.com>
2018-08-13 20:05:58 -0700

    UI for login

--- Nooves/View Controllers/LoginViewController.m
+++ Nooves/View Controllers/LoginViewController.m
@@ -27,45 +27,46 @@
     NSArray *colors = [NSArray arrayWithObjects:FlatPink, FlatSkyBlue, nil];
     self.view.backgroundColor = GradientColor(UIGradientStyleTopToBottom, self.view.frame, colors);
     
-    UILabel *nooves = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2-30, 120, 50, 5)];
+    UILabel *nooves = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/4-20, 150, 250, 70)];
     nooves.text = @"Nooves";
-    [nooves sizeToFit];
+    [nooves setFont:[UIFont fontWithName:@"ProximaNova-Semibold" size:70]];
+
     [self.view addSubview:nooves];
     
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     loginButton.delegate = self;
     loginButton.readPermissions = @[@"public_profile", @"email"];
     loginButton.center = self.view.center;
-    loginButton.frame = CGRectMake(self.view.bounds.size.width/6, 150, 250, 30);
+    loginButton.frame = CGRectMake(self.view.bounds.size.width/6, 240, 250, 30);
     
-    UILabel *or = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2-5, 190, 5, 5)];
+    UILabel *or = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2-8, 280, 5, 5)];
     or.text = @"or";
     [or sizeToFit];
     [self.view addSubview:or];
 
-    self.usernameField = [[UITextField alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 220, 250, 30)];
+    self.usernameField = [[UITextField alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 320, 250, 30)];
     self.usernameField.text = nil;
     self.usernameField.placeholder = @"Email or username";
     self.usernameField.borderStyle = UITextBorderStyleNone;
     self.usernameField.tintColor = [UIColor flatGrayColor];
-    self.usernameField.backgroundColor = [UIColor clearColor];
-    self.usernameField.layer.borderColor = [UIColor flatGrayColor].CGColor;
+    self.usernameField.backgroundColor = [UIColor flatWhiteColor];
+    self.usernameField.layer.borderColor = [UIColor flatBlackColor].CGColor;
     self.usernameField.layer.borderWidth = 2.0f;
     self.usernameField.layer.cornerRadius = 3.0f;
     [self.view addSubview:self.usernameField];
     
-    self.passwordField = [[UITextField alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 260, 250, 30)];
+    self.passwordField = [[UITextField alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 360, 250, 30)];
     self.passwordField.text = nil;
     self.passwordField.placeholder = @"Password";
     self.passwordField.borderStyle = UITextBorderStyleNone;
     self.passwordField.tintColor = [UIColor flatGrayColor];
-    self.passwordField.backgroundColor = [UIColor clearColor];
-    self.passwordField.layer.borderColor = [UIColor flatGrayColor].CGColor;
+    self.passwordField.backgroundColor = [UIColor flatWhiteColor];
+    self.passwordField.layer.borderColor = [UIColor flatBlackColor].CGColor;
     self.passwordField.layer.borderWidth = 2.0f;
     self.passwordField.layer.cornerRadius = 3.0f;
     [self.view addSubview:self.passwordField];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 620, 20, 5)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/6, 640, 20, 5)];
     label.text = @"Don't have an account? Sign up.";
     [label sizeToFit];
     [self.view addSubview:label];
@@ -131,12 +132,11 @@ didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result
 
 - (UIButton *)createLoginButton {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.frame = CGRectMake(self.view.bounds.size.width, 300, 250, 30);
-    button.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, -10);
+    button.frame = CGRectMake(self.view.bounds.size.width/6, 400, 250, 30);
     [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [button setTintColor:[UIColor blackColor]];
     [button setTitle:@"Login" forState:UIControlStateNormal];
-    [button setBackgroundColor:[UIColor flatSkyBlueColor]];
+    [button setBackgroundColor:[UIColor flatWhiteColor]];
     return button;
 }
 
